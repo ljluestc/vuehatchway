5d5be8936783dd449428fb4cb3813e88f3045f58
Matskevich <yanm@playtika.com>
2021-03-19 12:59:40 +0200

    Initial commit

---
+++ backend/src/main/java/macyan/org/english/helper/EnglishHelperApplication.java
@@ -0,0 +1,13 @@
+package macyan.org.english.helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishHelperApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/macyan/org/english/helper/EnglishHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package macyan.org.english.helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

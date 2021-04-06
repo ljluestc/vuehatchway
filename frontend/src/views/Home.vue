15fd8358b6edc536c873a1a226abb525f3423621
Matskevich <yanm@playtika.com>
2021-04-06 23:16:04 +0300

    Add Java redirect for index page for non existed routes

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,8 +2,8 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <Section type="Main vocabulary" src-data="/api/v1/translations/phrases" src-count="src-data"></Section>
-<!--    <Section type="Phrasal verbs" src-data="/test" src-count="src-data"></Section>-->
-<!--    <Section type="Useful phrases" src-data="/test" src-count="src-data"></Section>-->
+    <Section type="Phrasal verbs" src-data="/api/v1/translations/phrasal-verbs" src-count="src-data"></Section>
+    <Section type="Useful phrases" src-data="/api/v1/translations/words" src-count="src-data"></Section>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
